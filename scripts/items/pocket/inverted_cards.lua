3b4bb4ae64fa4266612328ce558d6ec05804f3f9
Alecontín <alecontin.xyz+github@gmail.com>
2024-07-21 07:27:49 +0300

    Updated ReplaceChance explanation

--- scripts/items/pocket/inverted_cards.lua
+++ scripts/items/pocket/inverted_cards.lua
@@ -1,6 +1,18 @@
 local cards = "scripts.items.pocket.inverted_cards."
 
+-- The chance that a card will be overwritten
+-- Cards might have a ReplaceChance inside their config, which will be used instead of this global chance
+-- If they do, there will be "--" next to the file name below
+local ReplaceChance = 0.25
+
 local files = {
+    --[[ Example of a card that has a ReplaceChance overwrite:
+    cards.."card_script", --
+                          ^^
+    If a card is marked that way, go inside the script and
+    search for "card.ReplaceChance = #.##" near the top, you can modify the value there
+    or simply delete or comment the line
+    ]]
     cards.."the_fool",
     cards.."the_magician",
     cards.."the_high_priestess",
@@ -26,10 +38,6 @@ local files = {
 }
 
 local extension = {}
-
--- The chance that a card will be overwritten
--- Cards might have a ReplaceChance inside their config, which will be used instead of this global chance
-local ReplaceChance = 0.25
 
 ---@param Mod ModReference
 function extension:init(Mod)
