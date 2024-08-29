8e58d0688b6e7ee2c6cd7a30470bb53b6881b9c6
Alecontín <alecontin.xyz+github@gmail.com>
2024-08-29 08:28:12 +0300

    Changed Inverted Tower to spawn Throwable Bombs

--- scripts/items/pocket/inverted_cards/the_tower.lua
+++ scripts/items/pocket/inverted_cards/the_tower.lua
@@ -8,24 +8,20 @@ card.Replace = Card.CARD_REVERSE_TOWER
 
 ---@param Mod ModReference
 function card:init(Mod)
-    local game = Game()
-
     ---@param player EntityPlayer
     Mod:AddCallback(ModCallbacks.MC_USE_CARD, function (_, _, player)
         local rng = player:GetCardRNG(card.ID)
-        local room = game:GetRoom()
 
         for _ = 1, rng:RandomInt(2, 4) do
-            local position = room:GetRandomPosition(50)
-            Isaac.Spawn(EntityType.ENTITY_MOVABLE_TNT, 1, 0, position, Vector.Zero, player)
+            local velocity = EntityPickup.GetRandomPickupVelocity(player.Position) / 2
+            Isaac.Spawn(EntityType.ENTITY_PICKUP, PickupVariant.PICKUP_THROWABLEBOMB, 0, player.Position, velocity, player)
         end
     end, card.ID)
 
     ---@class EID
     if EID then
-        local tnt = CollectibleType.COLLECTIBLE_MINE_CRAFTER
         EID:addCard(card.ID,
-            "#{{Collectible"..tnt.."}} Spawns 2-4 TNT"
+            "#{{Bomb}} Spawns 2-4 throwable bombs"
         )
     end
 end
