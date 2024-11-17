3d709db82e8660736871c79c76afd9fef36a60bc
Alecontín <alecontin.xyz+github@gmail.com>
2024-11-17 20:43:11 +0200

    Changed Inverted Empress functionality

--- scripts/items/pocket/inverted_cards/the_empress.lua
+++ scripts/items/pocket/inverted_cards/the_empress.lua
@@ -4,7 +4,7 @@
 
 
 
-local NUM_MOM_ITEMS_TO_GIVE = 5 -- *Default: `5` — The number of Mom items to give to the player*
+local NUM_MINIISAAC = 10 -- *Default: `10` — The number of MiniIsaacs to spawn*
 
 
 
@@ -28,40 +28,17 @@ card.Replace = Card.CARD_REVERSE_EMPRESS
 
 ---@param Mod ModReference
 function card:init(Mod)
-    local game = Game()
-    local ItemConfig = Isaac:GetItemConfig()
-
     ---@param player EntityPlayer
     Mod:AddCallback(ModCallbacks.MC_USE_CARD, function (_, _, player)
-        local rng = player:GetCardRNG(card.ID)
-
-        ---@diagnostic disable-next-line: param-type-mismatch
-        local mom_items = ItemConfig:GetTaggedItems(ItemTag.TAG_MOM)
-
-        helper.table.ShuffleTable(mom_items, rng)
-
-        for i = 1, NUM_MOM_ITEMS_TO_GIVE do
-            player:AddCollectibleEffect(mom_items[i].ID, true)
+        for _ = 1, NUM_MINIISAAC, 1 do
+            player:AddMinisaac(player.Position)
         end
-
-        local show = 1
-        Isaac.CreateTimer(function ()
-            local hud = game:GetHUD()
-            local item = mom_items[show]
-            hud:ShowItemText(player, item)
-
-            if item.Type == ItemType.ITEM_ACTIVE then
-                player:UseActiveItem(item.ID)
-            end
-
-            show = show + 1
-        end, 30, NUM_MOM_ITEMS_TO_GIVE, false)
     end, card.ID)
 
     ---@type EID
     if EID then
         EID:addCard(card.ID,
-            "#{{Mom}} Gives Isaac "..NUM_MOM_ITEMS_TO_GIVE.." random Mom item effects for the room"
+            "#{{Player0}} Spawns "..NUM_MINIISAAC.." MiniIsaacs"
         )
     end
 end
