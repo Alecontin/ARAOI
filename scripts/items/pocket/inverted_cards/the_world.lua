4714b8fbf7e8655212de7aecb0b178335df3a5e2
Alecontín <alecontin.xyz+github@gmail.com>
2024-11-17 21:31:20 +0200

    Inverted World now changes the level's crawlspace to lead to a black market

--- scripts/items/pocket/inverted_cards/the_world.lua
+++ scripts/items/pocket/inverted_cards/the_world.lua
@@ -8,27 +8,26 @@ card.Replace = Card.CARD_REVERSE_WORLD
 
 ---@param Mod ModReference
 function card:init(Mod)
+    local game = Game()
+
     ---@param player EntityPlayer
     Mod:AddCallback(ModCallbacks.MC_USE_CARD, function (_, _, player)
-        local rng = player:GetCardRNG(card.ID)
-        rng:RandomFloat()
+        local level = game:GetLevel()
+        local crawlspace = level:GetRoomByIdx(-4)
 
-        local room = RoomConfigHolder.GetRandomRoom(rng:GetSeed(), true, StbType.SPECIAL_ROOMS, RoomType.ROOM_BLACK_MARKET)
+        if crawlspace.VisitedCount == 0 then
+            local leading_to_blackmarket = RoomConfigHolder.GetRoomByStageTypeAndVariant(StbType.SPECIAL_ROOMS, RoomType.ROOM_DUNGEON, 1)
+            crawlspace.Data = leading_to_blackmarket
+        end
 
-        Isaac.ExecuteCommand("goto s.blackmarket."..room.Variant)
-
-        SaveData:CreateTimerInFrames("Schedule Spawn Inverted World Ladder", 1)
+        Isaac.GridSpawn(GridEntityType.GRID_STAIRS, 0, player.Position)
     end, card.ID)
-
-    Mod:AddCallback("Schedule Spawn Inverted World Ladder", function ()
-        local player = Isaac.GetPlayer()
-        Isaac.Spawn(EntityType.ENTITY_EFFECT, EffectVariant.TALL_LADDER, 0, player.Position, Vector.Zero, player)
-    end)
 
     ---@class EID
     if EID then
         EID:addCard(card.ID,
-            "#{{BlackSack}} Teleports Isaac to a random Black Market"
+            "#{{BlackSack}} Spawns a crawlspace leading to a Black Market"..
+            "#{{LadderRoom}} If the floor's crawlspace has already been visited, it will lead there instead"
         )
     end
 end
